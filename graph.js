211d931f3329fb22346283215240a4d3f8e1b334
etfagha oubeid <etfagha@softprofiles.com>
2020-05-15 23:51:47 +0000

    test add edje and add vertex

--- graph.js
+++ graph.js
@@ -9,18 +9,57 @@ class Graph {
         }
          
     }
-    removeVertex(vertex) {
+    addEdge(vertexOne, vertexTwo) {
+        console.log(this.adjacencyList[vertexTwo])
+        this.adjacencyList[vertexOne] =  this.adjacencyList[vertexOne].push(vertexTwo);
+        this.adjacencyList[vertexTwo] =  this.adjacencyList[vertexTwo].push(vertexOne);
         
     }
-    addEdge(vertex, edge) {
-        this.adjacencyList[vertex]=edge
+    removeEdge(vertexOne, vertexTwo) {
+        if (this.adjacencyList[vertexOne]) {
+           this.adjacencyList[vertexOne] =this.adjacencyList[vertex].filter(vertex=>vertex!==vertexTwo)
+        }
     }
+    removeVertex(vertexOne) {
+        let arr = this.adjacencyList[vertexOne]
+        arr.map(el => {
+            return this.adjacencyList[el].filter(v=>v !==vertexone)
+        })
+        delete this.adjacencyList[vertexOne]
+
+        while (this.adjacencyList[vertex].lenght) {
+            
+        }
+
+        
+    }
+   
 }
 
 let g = new Graph();
 g.addVertex("A");
 g.addVertex("B");
 g.addVertex("C");
-g.addEdge("A", ["B","C"])
+g.addVertex("D");
+g.addVertex("E");
+
+
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+
+// g.addEdge("B", "A")
+g.addEdge("B", "C");
+g.addEdge("B", "D")
+
+// g.addEdge("C", "B")
+g.addEdge("C", "D");
+
+
+// g.addEdge("D","B")
+// g.addEdge("D", "C");
+g.addEdge("D", "E")
+
+
  
 console.log(g)
